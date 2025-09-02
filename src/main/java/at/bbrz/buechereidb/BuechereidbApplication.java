a09538ccf029807891c635806229c792ac5154fc
ahwz <andreas.zincke@gmx.at>
2025-09-02 09:38:21 +0200

    init project

---
+++ src/main/java/at/bbrz/buechereidb/BuechereidbApplication.java
@@ -0,0 +1,13 @@
+package at.bbrz.buechereidb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuechereidbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuechereidbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/bbrz/buechereidb/BuechereidbApplicationTests.java
@@ -0,0 +1,13 @@
+package at.bbrz.buechereidb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuechereidbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
